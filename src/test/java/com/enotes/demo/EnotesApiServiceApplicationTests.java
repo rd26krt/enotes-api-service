ec067a0205b3106fd9bde39dab2558b53ade0736
rd26krt <dasr84931@gmail.com>
2024-12-29 18:01:16 +0530

    initial commit

---
+++ src/test/java/com/enotes/demo/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
